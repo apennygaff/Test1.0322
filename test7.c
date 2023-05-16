edaf25fc63e41d2775ddf1a6efbc5ced77953872
apennygaff <128569583+apennygaff@users.noreply.github.com>
2023-05-16 06:25:09 +0000

    switch문 조건문 끝

---
+++ test7.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+// main(){
+//   int x;
+//   printf("1:A | 2:B | 3:C\n");
+//   scanf("%d", &x);
+
+// if(x>=80)
+//   printf("A");
+// else if(x>=60)
+//   printf("B");
+// else if(x>=40)
+//   printf("C");
+//  printf("끝");
+// }
